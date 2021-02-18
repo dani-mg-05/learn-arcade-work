18b5af9613ea37dc7f2c8455b699931b81b2cdca
Daniel <daniel.medinag@edu.uah.es>
2021-02-18 19:19:22 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,4 @@
-print("It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
+print ("I want to be a great engineer.")
+print ("I have to work hard and \nI'm trying to do my best.")
+print ("But")
+print ("I know I'll get it.")
